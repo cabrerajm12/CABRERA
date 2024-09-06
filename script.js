f815975ea1933d8c79ae1515ebe60b2342151e6e
cabrerajm12 <josemariacabrera720@gmail.com>
2024-09-06 10:31:30 +0800

    Added Activity.txt

---
+++ script.js
@@ -0,0 +1,9 @@
+//Activity today
+
+let firstName = "Sander";
+let lastName = "Perejan";
+    
+console.log("HI Goodmorning Im " + firstName + " " + lastName + " 2nd year student");
+alert("HI im " + firstName + " " + lastName + "2nd year student ");
+
+document.getElementById("Text").innerHTML = "HI Goodmorning Im " + firstName + " " + lastName + " 2nd year student";
